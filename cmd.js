b0532365380e2363802064b592fe8c8f35917704
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-15 10:39:15 -0400

    feat: use filespy

--- cmd.js
+++ cmd.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-var usage = require('usage-and-quit')
-var usageFile = require('path').join(__dirname, 'USAGE.txt')
+var path = require('path')
 var argv = require('minimist')(process.argv.slice(2))
-var gaze = require('gaze')
-var spawn = require('child_process').spawn
 var files = argv._
 var cmd = argv.r
-if (!files) usage(usageFile)
-if (!cmd) usage(usageFile)
+if (!files || !cmd) {
+  var usage = require('usage-and-quit')
+  usage(path.join(__dirname, 'USAGE.txt'))
+}
+var spawn = require('child_process').spawn
 function spawnCommand () {
   var cmds = cmd.split(' ')
   var proc = spawn(cmds[0], cmds.slice(1), {
@@ -27,14 +27,53 @@ function killCommand () {
     process.kill(-proc.pid, 'SIGKILL')
   }
 }
-files.forEach(f => {
-  gaze(f, (err, watcher) => {
-    if (err) quit(err)
-    watcher.on('changed', () => {
-      killCommand()
-      proc = spawnCommand()
-    })
-  })
+function debounce(fn, ms) {
+  let t
+  return (...args) => {
+    clearTimeout(t)
+    t = setTimeout(() => fn(...args), ms)
+  }
+}
+var respawn = debounce(() => {
+  killCommand()
+  proc = spawnCommand()
+}, 16)
+function isDescendant(parent, file) {
+  return file == parent || file.startsWith(parent + '/')
+}
+var cwd = process.cwd()
+var only = []
+for (let file of files) {
+  const maybeHigher = file.startsWith('..') || path.isAbsolute(file)
+  file = path.resolve(file)
+  if (maybeHigher) {
+    const prevCwd = cwd
+    while (!isDescendant(cwd, file)) {
+      cwd = path.dirname(cwd)
+    }
+    if (cwd !== prevCwd) {
+      only.forEach((file, i) => {
+        only[i] = path.relative(cwd, path.join(prevCwd, file))
+      })
+    }
+  }
+  only.push(path.relative(cwd, file))
+}
+only.forEach((file, i) => {
+  only[i] = '/' + file
+})
+//console.log({cwd,only,})
+var {filespy} = require('filespy')
+var spy = filespy(cwd, {
+  only,
+  skip: ['node_modules', '.git'],
+}).on('change', () => {
+  respawn()
+}).on('error', err => {
+  console.error(err)
+  die()
+}).on('ready', () => {
+  //console.log(spy)
 })
 function die() {
   killCommand()
